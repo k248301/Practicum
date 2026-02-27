cfa8a317ade0e36996cf336b47462945a2146ff7
Syed Waqar Ali <k248301@nu.edu.pk>
2026-02-28 04:18:58 +0500

    Added MT5 Config

--- MarketDataAPI/config.py
+++ MarketDataAPI/config.py
@@ -1,7 +1,7 @@
 class Config:
-    MT5_PATH = "xyz/abc/efg"
-    ACC_ID = 1234567890
-    ACC_KEY = "3t47wjsu2"
-    ACC_ORG = ""
-    TICKERS = []
+    MT5_PATH = "C:/Program Files/MetaTrader 5/terminal64.exe"
+    ACC_ID = 2121951337
+    ACC_KEY = "AUx*Q7FC6$"
+    ACC_ORG = "XBTFX-MetaTrader5"
+    TICKERS = ['BTCUSD!', 'ETHUSD!', 'XRPUSD!', 'LTCUSD!', 'SOLUSD!', 'BNBUSD!', 'DOTUSD!', 'DOGUSD!', 'TETUSD!']
     PORT = 8080
